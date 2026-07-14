d4be949622aa6ea08e6d4a0d964928aaf1c23beb
joel jacob <joelj712@gmail.com>
2026-07-14 23:44:03 +0530

    view all comp

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<AddTeam />} />
+           <Route path='/view' element={<ViewTeams />} />
         </Routes>
       </BrowserRouter>
 
